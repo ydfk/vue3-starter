aafe6fb952a70c5aed201a7269c2c63c2be20beb
ydfk <lyh6728326@gmail.com>
2021-08-26 18:53:57 +0800

    chore(other): 修改commitlint

--- .cz-config.js
+++ .cz-config.js
@@ -3,30 +3,27 @@
  * @Author: ydfk
  * @Date: 2021-08-25 10:19:43
  * @LastEditors: ydfk
- * @LastEditTime: 2021-08-26 17:15:36
+ * @LastEditTime: 2021-08-26 18:52:30
  */
 module.exports = {
   // type 类型（定义之后，可通过上下键选择）
   types: [
-    { value: "feat", name: "feat:     新增功能" },
-    { value: "fix", name: "fix:      修复 bug" },
-    { value: "docs", name: "docs:     文档变更" },
-    { value: "style", name: "style:    代码格式（不影响功能，例如空格、分号等格式修正）" },
-    { value: "refactor", name: "refactor: 代码重构（不包括 bug 修复、功能新增）" },
-    { value: "perf", name: "perf:     性能优化" },
-    { value: "test", name: "test:     添加、修改测试用例" },
+    { value: "feat", name: "🌟feat:     新增功能" },
+    { value: "fix", name: "🐛fix:      修复 bug" },
+    { value: "docs", name: "📝docs:     文档变更" },
+    { value: "style", name: "🎨style:    代码格式（不影响功能，例如空格、分号等格式修正）" },
+    { value: "refactor", name: "🔨refactor: 代码重构（不包括 bug 修复、功能新增）" },
+    { value: "perf", name: "🚀perf:     性能优化" },
+    { value: "test", name: "🔧test:     添加、修改测试用例" },
     { value: "build", name: "build:    构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）" },
-    { value: "ci", name: "ci:       修改 CI 配置、脚本" },
-    { value: "chore", name: "chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）" },
-    { value: "revert", name: "revert:   回滚 commit" },
-    { value: "release", name: "release:   发布" },
+    { value: "ci", name: "📦ci:       修改 CI 配置、脚本" },
+    { value: "chore", name: "🏠chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）" },
+    { value: "revert", name: "🔙revert:   回滚 commit" },
   ],
 
   // scope 类型（定义之后，可通过上下键选择）
   scopes: [
     ["components", "组件相关"],
-    ["hooks", "hook 相关"],
-    ["utils", "utils 相关"],
     ["ui", "对 ui组件 的调整"],
     ["styles", "样式相关"],
     ["deps", "项目依赖"],
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,17 +3,4 @@
 ### Features
 
 - **components:** add login and home ([ec8a485](https://github.com/ydfk/vue3-starter/commit/ec8a48525f3c444ebe99e9c8d27a33083ee295c9))
-
-<!--
- * @Description: Copyright (c) ydfk. All rights reserved
- * @Author: ydfk
- * @Date: 2021-08-25 10:19:43
- * @LastEditors: ydfk
- * @LastEditTime: 2021-08-26 16:55:08
--->
-
-## 0.0.1 (2021-08-26)
-
-### Features
-
-- **components:** add login and home ([ec8a485](https://github.com/ydfk/vue3-starter/commit/ec8a48525f3c444ebe99e9c8d27a33083ee295c9))
+- **deps:** add pinia ([216a72d](https://github.com/ydfk/vue3-starter/commit/216a72dcacd2c38f36213b66c35274c3ac99e0b0))
